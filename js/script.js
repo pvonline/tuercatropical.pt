21aa1d9e9de0e4a8d87107a938d3b59aa2b48680
lucaspalaciopimienta <radioelpalaciovallenato@gmail.com>
2024-09-02 20:44:34 -0500

    luta

---
+++ js/script.js
@@ -0,0 +1,36 @@
+
+/*_________Inicio Reloj Digital_______*/
+
+const $tiempo = document.querySelector('.tiempo'),
+$fecha = document.querySelector('.fecha');
+
+function digitalClock(){
+    let f = new Date(),
+    dia = f.getDate(),
+    mes = f.getMonth() + 1,
+    anio = f.getFullYear(),
+    diaSemana = f.getDay();
+
+    dia = ('0' + dia).slice(-2);
+    mes = ('0' + mes).slice(-2)
+
+    let timeString = f.toLocaleTimeString();
+    $tiempo.innerHTML = timeString;
+
+    let semana = ['Domingo','Lunes','Martes','Miercoles','Jueves','Viernes','Sabado'];
+    let showSemana = (semana[diaSemana]);
+    $fecha.innerHTML = `${showSemana}  ${dia}-${mes}-${anio}`
+}
+setInterval(() => {
+    digitalClock()
+}, 1000);
+
+/*______Final Reloj Digital________*/
+
+
+
+
+
+
+
+
